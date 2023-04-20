07b7de3037371f8b7739bf7dc84824a086017986
serkandogaan <serkan.dogan333@outlook.com>
2023-04-20 23:13:50 +0300

    rover classı eklendi

---
+++ HBTST/Rover.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBTST
+{
+    public class Rover
+    {
+        public int X;
+        public int Y;
+        public string Direction;
+
+        public Rover(int x, int y, string direction)
+        {
+            X = x;
+            Y = y;
+            Direction = direction;
+        }
+    }
+}
